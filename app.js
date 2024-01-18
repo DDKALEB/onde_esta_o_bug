d802bab45291611ee19fddeea5fdf0adbb60ea7c
Claudio Tavares da Silva <engclaudiosilva@gmail.com>
2024-01-18 11:26:55 -0300

    Versão Inicial 170124

---
+++ app.js
@@ -0,0 +1,15 @@
+/*
+let senhaDoSistema = 'senhaTeste!';
+
+let senha = prompt('Digite a senha do sistema:');
+
+if (senha == senhaDoSistema) {
+  alert('Acesso ao sistema garantido');
+} else alert('SEM Acesso ao sistema garantido');
+*/
+let contador = 1;
+
+while (contador < 4) {
+  console.log('Executando a iteração ' + contador);
+  contador = contador + 1;
+}
